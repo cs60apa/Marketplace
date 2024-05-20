b14ea00d1eb7907e54cde0a7ff2e64bca477bcf8
marksikaundi <emmanuelmark117@gmail.com>
2024-05-20 21:51:57 +0200

    setting up configs

--- api/app.js
+++ api/app.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}))
+
+
+
+
+module.exports = app;
