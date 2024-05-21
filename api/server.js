c7219575dcff6bc27193866b97ea71142710ff29
marksikaundi <emmanuelmark117@gmail.com>
2024-05-21 16:45:36 +0200

    chore: Connect to MongoDB in server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,22 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+const connectToMongo = () => {
+  const url = process.env.MONGODB_CS;
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log("Connected to MongoDB " + data.connection.host);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+connectToMongo();
 
 const port = process.env.PORT || 8000;
 
