523051b82fca64ee14ae16a5cbae846a32410bb5
marksikaundi <emmanuelmark117@gmail.com>
2024-05-26 17:25:38 +0200

    chore: Refactor authController.js for user login functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 
 // Register a new user
 exports.register = async (req, res) => {
@@ -17,13 +18,44 @@ exports.register = async (req, res) => {
 
 // Login user
 exports.login = async (req, res) => {
+  const { email, password } = req.body;
   // 1. get user from db using email
+  const dbUser = await User.findOne({ email });
 
-  // 2. check if user exists 
+  // 2. check if user exists
+  if (!dbUser) {
+    res.status(404).json({
+      success: false,
+      message: "We do not have a user with that email",
+    });
+  } else {
+    // 3. compare passwords
+    const comparePassword = await dbUser.comparePassword(
+      password,
+      dbUser.password
+    );
 
-  // 3. compare passwords
+    if (comparePassword) {
+      // 4. create a jsonwebtoken
+      const token = jwt.sign(
+        {
+          id: dbUser._id,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.JWT_SECRET_EXPIRES,
+        }
+      );
 
-  // 4. create a jsonwebtoken
-
-
+      res.status(200).json({
+        success: true,
+        token,
+      });
+    }else{
+      res.status(400).json({
+        success: false,
+        message: "Password or Email is incorrect"
+      })
+    }
+  }
 };
