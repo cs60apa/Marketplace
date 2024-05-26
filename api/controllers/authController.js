22af283eafca772bfab4803e69563cd1b433564b
marksikaundi <emmanuelmark117@gmail.com>
2024-05-26 17:11:10 +0200

    feat: Implementing user login functionality layout

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -14,3 +14,16 @@ exports.register = async (req, res) => {
     res.status(500).json({ success: false, error });
   }
 };
+
+// Login user
+exports.login = async (req, res) => {
+  // 1. get user from db using email
+
+  // 2. check if user exists 
+
+  // 3. compare passwords
+
+  // 4. create a jsonwebtoken
+
+
+};
