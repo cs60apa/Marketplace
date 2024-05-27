3f3fb90c701486af83cb19f5a894e72458210f10
marksikaundi <emmanuelmark117@gmail.com>
2024-05-28 01:59:21 +0200

    chore: Refactor authController.js for user login functionality and error handling improvements

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,69 +1,54 @@
 const User = require("../model/userModel");
 const jwt = require("jsonwebtoken");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/AppError");
 
 // Register a new user
-exports.register = async (req, res) => {
+exports.register = catchAsync(async (req, res, next) => {
   const { email, storename, password, name } = req.body;
 
-  try {
-    const user = await User.create({ email, storename, password, name });
-    res.status(200).json({
-      success: true,
-      user,
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, error });
-  }
-};
+  const user = await User.create({ email, storename, password, name });
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
 // Login user
-exports.login = async (req, res) => {
+exports.login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
 
-  try {
-    // 1. get user from db using email
-    const dbUser = await User.findOne({ email }).select("+password");
+  // 1. get user from db using email
+  const dbUser = await User.findOne({ email }).select("+password");
 
-    // 2. check if user exists
-    if (!dbUser) {
-      return res.status(404).json({
-        success: false,
-        message: "We do not have a user with that email",
-      });
-    }
+  // 2. check if user exists
+  if (!dbUser) {
+    next(new AppError("We do not have a user with that email", 404));
+  }
 
-    // 3. compare passwords
-    const comparePassword = await dbUser.comparePassword(
-      password,
-      dbUser.password
+  // 3. compare passwords
+  const comparePassword = await dbUser.comparePassword(
+    password,
+    dbUser.password
+  );
+
+  if (comparePassword) {
+    // 4. create a jsonwebtoken
+    const token = jwt.sign(
+      {
+        id: dbUser._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_SECRET_EXPIRES,
+      }
     );
 
-    if (comparePassword) {
-      // 4. create a jsonwebtoken
-      const token = jwt.sign(
-        {
-          id: dbUser._id,
-        },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: process.env.JWT_SECRET_EXPIRES,
-        }
-      );
-
-      return res.status(200).json({
-        success: true,
-        token,
-      });
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "Password or Email is incorrect",
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: "Something went wrong, please try again later",
+    return res.status(200).json({
+      success: true,
+      token,
     });
+  } else {
+    next(new AppError("Password or Email is incorrect", 400));
   }
-};
+});
